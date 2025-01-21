883566be118849fcc422ea9f125c0949cb0ca5d9
Anton Lindström <anton.jumkil@gmail.com>
2025-01-21 11:02:23 +0100

    progress

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 var word1 = "CODING";
 var word2 = "PC";
 
+var player1WrongGuesses = 0;
+var player2WrongGuesses = 0;
+
 var word1hidden = "";
 for (let i = 0; i < word1.length; i++) {
     word1hidden += "_ ";
@@ -22,14 +25,27 @@ Guess = (event) => {
         if(word2.includes(document.getElementById("player1guess").value)){
             console.log("hej");
         }else{
-            document.getElementById("player2guesses").innerHTML += document.getElementById("player1guess").value;
+            if(player1WrongGuesses == 5){
+                document.getElementById("player2guesses").innerHTML += " Spelare 1 förlorade"
+            } else{
+                player1WrongGuesses++;
+                document.getElementById("player2guesses").innerHTML += document.getElementById("player1guess").value;
+                document.getElementById("player2img").src = `/images/${player1WrongGuesses + 1}.png`;
+            }
         }
         document.getElementById("player1guess").value = "";
+
     } else if(event.target.id == "player2form"){
         if(word2.includes(document.getElementById("player2guess").value)){
             console.log("hej");
         }else{
-            document.getElementById("player1guesses").innerHTML += document.getElementById("player2guess").value;
+            if(player2WrongGuesses == 5){
+                document.getElementById("player1guesses").innerHTML += " Spelare 2 förlorade"
+            } else{
+                player2WrongGuesses++;
+                document.getElementById("player1guesses").innerHTML += document.getElementById("player2guess").value;
+                document.getElementById("player1img").src = `/images/${player2WrongGuesses + 1}.png`;
+            }
         }
         document.getElementById("player2guess").value = "";
     }
